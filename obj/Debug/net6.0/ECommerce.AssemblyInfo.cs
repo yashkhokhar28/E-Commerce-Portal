24628b7e6deba4fdd97b861ee298b343e71b345d
yashkhokhar28 <yashkhokhar28@gmail.com>
2024-03-15 18:46:13 +0530

    UI Fixed

--- obj/Debug/net6.0/ECommerce.AssemblyInfo.cs
+++ obj/Debug/net6.0/ECommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edcab557929e994a8c2dabafaa4328d91161a2d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a57bdc535f35865bc4f87a3cb31720dbc0f6e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
